0664919b47d69136134b33a77271f03d1dbfa9f0
navas <gabrielnavas@email.com>
2021-08-30 23:19:10 -0300

    Thinkful - String Drills: Quotable

---
+++ javascript/Thinkful - String Drills: Quotable.js
@@ -0,0 +1,9 @@
+// Thinkful - String Drills: Quotable
+
+const quotable = (name, quote) => `${name} said: "${quote}"`
+
+console.log(quotable('Grae', 'Practice makes perfect') ==='Grae said: "Practice makes perfect"')
+console.log(quotable('Dan', 'Get back to work, Grae') === 'Dan said: "Get back to work, Grae"')
+console.log(quotable('Alex', 'Ruby is great fun') === 'Alex said: "Ruby is great fun"')
+console.log(quotable('Bethany', 'Yes, way more fun than R') === 'Bethany said: "Yes, way more fun than R"')
+console.log(quotable('Darrell', 'What the heck is this thing?') === 'Darrell said: "What the heck is this thing?"')
